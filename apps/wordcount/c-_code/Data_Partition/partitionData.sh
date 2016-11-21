33c5f2da4791d7a8d9232df180410081551cbe45
JustL <lingysjustinas@gmail.com>
2016-11-21 16:21:45 +0800

    Update partitionData.sh

--- apps/wordcount/c-_code/Data_Partition/partitionData.sh
+++ apps/wordcount/c-_code/Data_Partition/partitionData.sh
@@ -3,13 +3,16 @@
 # Run dataset partition and measure how much time it takes
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MEASURE_FILE="${THIS_DIR}/time_to_partition.txt"
+MEASURE_FILE="${THIS_DIR}/time_to_partition.csv"
 PARTITION_SCRIPT="${THIS_DIR}/splitData"
+INIT_SCRIPT="${THIS_DIR}/initLogFile"
 
+# Initialize the log file
+$INIT_SCRIPT $MEASURE_FILE
 
-#SIZES=( "100MB" "200MB" "500MB" "1GB" "2GB" "5GB" )
-SIZES=( "100MB" "200MB" )
-CHUNK_SIZES=( "16" "20" ) # 1500 bytes and 9000 bytes
+SIZES=( "100MB" "200MB" "500MB" "1GB" "2GB" "5GB" )
+#SIZES=( "100MB" "200MB" )
+CHUNK_SIZES=( "1500" "9000" ) # 1500 bytes and 9000 bytes
 
 
 for s in "${SIZES[@]}"
